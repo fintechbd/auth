810760e5f4efd4f104857702ef9369f25b370946
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-09-22 03:32:54 +0600

    sanctum integration start

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use Fintech\Auth\Http\Controllers\AuthenticatedSessionController;
+use Fintech\Auth\Http\Controllers\EmailVerificationNotificationController;
+use Fintech\Auth\Http\Controllers\NewPasswordController;
+use Fintech\Auth\Http\Controllers\PasswordResetLinkController;
+use Fintech\Auth\Http\Controllers\RegisteredUserController;
+use Fintech\Auth\Http\Controllers\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +21,33 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::prefix('auth')->group(function () {
+        Route::post('/register', [RegisteredUserController::class, 'store'])
+            ->middleware('guest')
+            ->name('register');
 
+        Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+            ->middleware('guest')
+            ->name('login');
+
+        Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+            ->middleware('guest')
+            ->name('password.email');
+
+        Route::post('/reset-password', [NewPasswordController::class, 'store'])
+            ->middleware('guest')
+            ->name('password.store');
+
+        Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
+            ->middleware(['auth', 'signed', 'throttle:6,1'])
+            ->name('verification.verify');
+
+        Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+            ->middleware(['auth', 'throttle:6,1'])
+            ->name('verification.send');
+
+        Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+            ->middleware('auth')
+            ->name('logout');
     });
 });
 
---
+++ src/Http/Controllers/RegisteredUserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Fintech\Auth\Http\Controllers;
+
+use App\Models\User;
+use Fintech\Auth\Http\Requests\RegistrationRequest;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Handle an incoming registration request.
+     *
+     * @param RegistrationRequest $request
+     * @return Response
+     */
+    public function store(RegistrationRequest $request): Response
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return response()->noContent();
+    }
+}

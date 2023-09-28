80d55d15dc69db447a7f10a7510fd22ed0bd7c8f
hafijul233 <hafijul233@users.noreply.github.com>
2023-09-28 23:19:23 +0000

    Fix styling

--- database/migrations/2023_09_28_230630_create_teams_table.php
+++ database/migrations/2023_09_28_230630_create_teams_table.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Spatie\Permission\PermissionRegistrar;
 
-return new class extends Migration {
-
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -17,7 +17,7 @@ return new class extends Migration {
         $tableNames = config('permission.table_names');
         $columnNames = config('permission.column_names');
 
-        if (!$teams) {
+        if (! $teams) {
             return;
         }
 
@@ -42,7 +42,7 @@ return new class extends Migration {
             $table->timestamp('restored_at')->nullable();
         });
 
-        if (!Schema::hasColumn($tableNames['roles'], $columnNames['team_foreign_key'])) {
+        if (! Schema::hasColumn($tableNames['roles'], $columnNames['team_foreign_key'])) {
             Schema::table($tableNames['roles'], function (Blueprint $table) use ($columnNames) {
                 $table->foreignId($columnNames['team_foreign_key'])->nullable()->after('id');
                 $table->dropUnique('roles_name_guard_name_unique');
@@ -50,7 +50,7 @@ return new class extends Migration {
             });
         }
 
-        if (!Schema::hasColumn($tableNames['model_has_permissions'], $columnNames['team_foreign_key'])) {
+        if (! Schema::hasColumn($tableNames['model_has_permissions'], $columnNames['team_foreign_key'])) {
             Schema::table($tableNames['model_has_permissions'], function (Blueprint $table) use ($tableNames, $columnNames) {
                 $table->foreignId($columnNames['team_foreign_key'])->default('1');
 
@@ -67,7 +67,7 @@ return new class extends Migration {
             });
         }
 
-        if (!Schema::hasColumn($tableNames['model_has_roles'], $columnNames['team_foreign_key'])) {
+        if (! Schema::hasColumn($tableNames['model_has_roles'], $columnNames['team_foreign_key'])) {
             Schema::table($tableNames['model_has_roles'], function (Blueprint $table) use ($tableNames, $columnNames) {
                 $table->foreignId($columnNames['team_foreign_key'])->default('1');
                 if (DB::getDriverName() !== 'sqlite') {
--- src/Http/Middleware/TeamsPermission.php
+++ src/Http/Middleware/TeamsPermission.php
@@ -10,13 +10,13 @@ class TeamsPermission
     /**
      * Handle an incoming request.
      *
-     * @param Request $request
-     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      * @return Response
      */
     public function handle($request, \Closure $next)
     {
-        if (!empty(auth('api')->user())) {
+        if (! empty(auth('api')->user())) {
             setPermissionsTeamId(auth('api')->user()->getTeamIdFromToken());
         }
 

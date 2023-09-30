0ebc002ea5b6ee90602ec777b5967df20f62d555
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-10-01 05:56:33 +0600

    auth route version tag removed

--- routes/api.php
+++ routes/api.php
@@ -19,42 +19,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('v1')->group(function () {
-    Route::prefix('auth')->group(function () {
-        Route::post('/register', [RegisteredUserController::class, 'store'])
-            ->middleware('guest')
-            ->name('register');
+Route::prefix('auth')->name('auth.')->group(function () {
+    Route::post('/register', [RegisteredUserController::class, 'store'])
+        ->middleware('guest')
+        ->name('register');
 
-        Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-            ->middleware('guest')
-            ->name('login');
+    Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+        ->middleware('guest')
+        ->name('login');
 
-        Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
-            ->middleware('guest')
-            ->name('password.email');
+    Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+        ->middleware('guest')
+        ->name('password.email');
 
-        Route::post('/reset-password', [NewPasswordController::class, 'store'])
-            ->middleware('guest')
-            ->name('password.store');
+    Route::post('/reset-password', [NewPasswordController::class, 'store'])
+        ->middleware('guest')
+        ->name('password.store');
 
-        Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
-            ->middleware(['auth', 'signed', 'throttle:6,1'])
-            ->name('verification.verify');
+    Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
+        ->middleware(['auth', 'signed', 'throttle:6,1'])
+        ->name('verification.verify');
 
-        Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-            ->middleware(['auth', 'throttle:6,1'])
-            ->name('verification.send');
+    Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+        ->middleware(['auth', 'throttle:6,1'])
+        ->name('verification.send');
 
-        Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-            ->middleware(config('fintech.auth.middleware'))
-            ->name('logout');
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+        ->middleware(config('fintech.auth.middleware'))
+        ->name('logout');
 
-        Route::middleware(config('fintech.auth.middleware'))->group(function () {
-            Route::apiResource('users', \Fintech\Auth\Http\Controllers\UserController::class);
-            //        Route::apiResource('roles', \Fintech\Auth\Http\Controllers\RoleController::class);
-            //        Route::apiResource('permissions', \Fintech\Auth\Http\Resources\PermissionCollection::class);
-            //        Route::apiResource('teams', \Fintech\Auth\Http\Controllers\TeamController::class);
-            Route::apiSingleton('users.profile', \Fintech\Auth\Http\Controllers\ProfileController::class);
-        });
+    Route::middleware(config('fintech.auth.middleware'))->group(function () {
+        Route::apiResource('users', \Fintech\Auth\Http\Controllers\UserController::class);
+        //        Route::apiResource('roles', \Fintech\Auth\Http\Controllers\RoleController::class);
+        //        Route::apiResource('permissions', \Fintech\Auth\Http\Resources\PermissionCollection::class);
+        //        Route::apiResource('teams', \Fintech\Auth\Http\Controllers\TeamController::class);
+        Route::apiSingleton('users.profile', \Fintech\Auth\Http\Controllers\ProfileController::class);
     });
 });

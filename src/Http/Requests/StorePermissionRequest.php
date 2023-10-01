77dfe0211a3a97d1f3bfaef6c573a8ed6fc82484
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-10-01 22:10:35 +0600

    LP-27 feat: permission create name unique rule added

--- src/Http/Requests/StorePermissionRequest.php
+++ src/Http/Requests/StorePermissionRequest.php
@@ -2,6 +2,7 @@
 
 namespace Fintech\Auth\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -22,8 +23,10 @@ class StorePermissionRequest extends FormRequest
      */
     public function rules(): array
     {
+        $uniqueRule = 'unique:' . config('fintech.auth.permission_model', \Fintech\Auth\Models\Permission::class) . ',name';
+
         return [
-            'name' => ['required', 'string', 'min:5', 'max:255'],
+            'name' => ['required', 'string', 'min:5', 'max:255', $uniqueRule],
             'guard_name' => ['required', 'string', Rule::in(array_keys(config('auth.guards', ['web', 'api'])))],
         ];
     }

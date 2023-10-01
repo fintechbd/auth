2ac5df39271ca5830fd97eff571b7cc89dcd8768
hafijul233 <hafijul233@users.noreply.github.com>
2023-10-01 16:11:55 +0000

    Fix styling

--- src/Http/Requests/StorePermissionRequest.php
+++ src/Http/Requests/StorePermissionRequest.php
@@ -2,7 +2,6 @@
 
 namespace Fintech\Auth\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -23,7 +22,7 @@ class StorePermissionRequest extends FormRequest
      */
     public function rules(): array
     {
-        $uniqueRule = 'unique:' . config('fintech.auth.permission_model', \Fintech\Auth\Models\Permission::class) . ',name';
+        $uniqueRule = 'unique:'.config('fintech.auth.permission_model', \Fintech\Auth\Models\Permission::class).',name';
 
         return [
             'name' => ['required', 'string', 'min:5', 'max:255', $uniqueRule],

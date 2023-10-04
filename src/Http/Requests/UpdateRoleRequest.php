e14922d7393b788823ca3497d8dc4ae43f900ac0
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-10-05 02:58:16 +0600

    LP-18 feat: role crud complete

--- src/Http/Requests/UpdateRoleRequest.php
+++ src/Http/Requests/UpdateRoleRequest.php
@@ -26,6 +26,7 @@ class UpdateRoleRequest extends FormRequest
 
         return [
             'name' => ['required', 'string', 'min:5', 'max:255', $uniqueRule],
+            'team_id' => ['required', 'integer'],
             'guard_name' => ['required', 'string', Rule::in(array_keys(config('auth.guards', ['web', 'api'])))],
             'permissions' => ['nullable', 'array'],
             'permissions.*' => ['nullable', 'integer'],

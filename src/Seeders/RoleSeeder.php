64871b43a141a1576d0d07e78ceeab27f718076d
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-10-06 00:17:28 +0600

    basic route seeders added

---
+++ src/Seeders/RoleSeeder.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Fintech\Auth\Seeders;
+
+use Fintech\Auth\Facades\Auth;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        foreach ($this->data() as $role) {
+            Auth::role()->create($role);
+        }
+    }
+
+    private function data()
+    {
+        return [
+            [
+                'id' => '1',
+                'name' => 'Super Admin',
+                'team_id' => 1,
+                'creator_id' => null,
+                'editor_id' => null,
+                'destroyer_id' => null,
+                'created_at' => '2023-08-14 13:11:03',
+                'updated_at' => '2023-08-14 13:11:03',
+            ],
+            [
+                'id' => '2',
+                'name' => 'Admin',
+                'team_id' => 1,
+                'creator_id' => null,
+                'editor_id' => null,
+                'destroyer_id' => null,
+                'created_at' => '2023-08-14 13:11:03',
+                'updated_at' => '2023-08-14 13:11:03',
+            ],
+            [
+                'id' => '3',
+                'name' => 'Executive',
+                'team_id' => 1,
+                'creator_id' => null,
+                'editor_id' => null,
+                'destroyer_id' => null,
+                'created_at' => '2023-08-14 13:11:03',
+                'updated_at' => '2023-08-14 13:11:03',
+            ],
+            [
+                'id' => '4',
+                'name' => 'Customer',
+                'team_id' => 1,
+                'creator_id' => null,
+                'editor_id' => null,
+                'destroyer_id' => null,
+                'created_at' => '2023-08-14 13:11:03',
+                'updated_at' => '2023-08-14 13:11:03',
+            ],
+        ];
+    }
+}
---
+++ src/Seeders/TeamSeeder.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Fintech\Auth\Seeders;
+
+use Fintech\Auth\Facades\Auth;
+use Illuminate\Database\Seeder;
+
+class TeamSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        foreach ($this->data() as $team) {
+            Auth::team()->create($team);
+        }
+    }
+
+    private function data()
+    {
+        return [
+            [
+                'id' => '1',
+                'name' => 'Bangladesh',
+                'creator_id' => null,
+                'editor_id' => null,
+                'destroyer_id' => null,
+                'created_at' => '2023-08-14 13:11:03',
+                'updated_at' => '2023-08-14 13:11:03',
+            ],
+            [
+                'id' => '2',
+                'name' => 'India',
+                'creator_id' => null,
+                'editor_id' => null,
+                'destroyer_id' => null,
+                'created_at' => '2023-08-14 13:11:03',
+                'updated_at' => '2023-08-14 13:11:03',
+            ],
+            [
+                'id' => '3',
+                'name' => 'China',
+                'creator_id' => null,
+                'editor_id' => null,
+                'destroyer_id' => null,
+                'created_at' => '2023-08-14 13:11:03',
+                'updated_at' => '2023-08-14 13:11:03',
+            ],
+            [
+                'id' => '4',
+                'name' => 'Malaysia',
+                'creator_id' => null,
+                'editor_id' => null,
+                'destroyer_id' => null,
+                'created_at' => '2023-08-14 13:11:03',
+                'updated_at' => '2023-08-14 13:11:03',
+            ],
+            [
+                'id' => '5',
+                'name' => 'Philippines',
+                'creator_id' => null,
+                'editor_id' => null,
+                'destroyer_id' => null,
+                'created_at' => '2023-08-14 13:11:03',
+                'updated_at' => '2023-08-14 13:11:03',
+            ]
+        ];
+    }
+}

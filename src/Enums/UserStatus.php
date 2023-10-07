1000a30cf2377369b790d0ec3f31d646b4334589
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-10-08 00:47:16 +0600

    use status enum typo fixed

--- src/Enums/UserStatus.php
+++ src/Enums/UserStatus.php
@@ -5,7 +5,7 @@ namespace Fintech\Auth\Enums;
 enum UserStatus: string
 {
     case Active = 'ACTIVE';
-    case InActive = 'IN-ACTIVE';
+    case InActive = 'INACTIVE';
     case Banned = 'BANNED';
     case Flagged = 'FLAGGED';
     case Terminated = 'TERMINATED';

cb27f0df06d2fb76ddd5ccac3f31fbd2ea8abddb
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-10-14 05:10:12 +0600

    LP-102 Test that the email field is in the correct format when registration

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -3,6 +3,86 @@
 
 use function Pest\Laravel\postJson;
 
+/*test('Test that the email field is in the correct format when registration', function () {
+    $registration = postJson('/api/auth/register', [
+        "name" => "MT TECHNOLOGIES",
+        "mobile" => "01700000001",
+        "email" => "admin1@mt-technologies.com",
+        "login_id" => "01700000001",
+        "pin" => "123456",
+        "password" => "12345678",
+        "app_version" => "0.0.1",
+        "language" => "bd",
+        "currency" => "BDT",
+        "father_name" => "MT",
+        "mother_name" => "TECHNOLOGIES",
+        "gender" => "male",
+        "marital_status" => "unmarried",
+        "occupation" => "service",
+        "source_of_income" => "salary",
+        "id_type" => "passport",
+        "id_no" => "1234567890",
+        "id_issue_country" => "BANGLADESH",
+        "id_expired_at" => "2030-12-31",
+        "id_issue_at" => "2020-01-01",
+        "date_of_birth" => "1985-02-19",
+        "permanent_address" => "DHAKA",
+        "city_id" => "1",
+        "state_id" => "1",
+        "country_id" => "18",
+        "post_code" => "1100",
+        "present_address" => "DHAKA",
+        "present_city_id" => "1",
+        "present_state_id" => "1",
+        "present_country_id" => "18",
+        "present_post_code" => "1100",
+        "nationality" => "BANGLADESHI"
+    ]);
+    //dd($registration['message']);
+    //$this->assertSame('The email field must be a valid email address.', $registration['message']);
+    expect($registration['message'])->toBe('The email field must be a valid email address.');
+    //assertStatus(201);
+});
+test('Test that the email address is in the correct format when registration', function () {
+    $registration = postJson('/api/auth/register', [
+        "name" => "MT TECHNOLOGIES",
+        "mobile" => "01700000001",
+        "email" => "admin1@mt-technologies.com",
+        "login_id" => "01700000001",
+        "pin" => "123456",
+        "password" => "12345678",
+        "app_version" => "0.0.1",
+        "language" => "bd",
+        "currency" => "BDT",
+        "father_name" => "MT",
+        "mother_name" => "TECHNOLOGIES",
+        "gender" => "male",
+        "marital_status" => "unmarried",
+        "occupation" => "service",
+        "source_of_income" => "salary",
+        "id_type" => "passport",
+        "id_no" => "1234567890",
+        "id_issue_country" => "BANGLADESH",
+        "id_expired_at" => "2030-12-31",
+        "id_issue_at" => "2020-01-01",
+        "date_of_birth" => "1985-02-19",
+        "permanent_address" => "DHAKA",
+        "city_id" => "1",
+        "state_id" => "1",
+        "country_id" => "18",
+        "post_code" => "1100",
+        "present_address" => "DHAKA",
+        "present_city_id" => "1",
+        "present_state_id" => "1",
+        "present_country_id" => "18",
+        "present_post_code" => "1100",
+        "nationality" => "BANGLADESHI"
+    ]);
+    //dd($registration['message']);
+    //$this->assertSame('The email field must be a valid email address.', $registration['message']);
+    expect($registration['message'])->toBe('The email field must be a valid email address.');
+    //assertStatus(201);
+});*/
 test('Enter an email address with special characters in the email field when registration', function () {
     $registration = postJson('/api/auth/register', [
         "name" => "MT TECHNOLOGIES",
@@ -123,7 +203,7 @@ test('Leave the email field blank when registration', function () {
     expect($registration['message'])->toBe('The email field is required.');
     //assertStatus(201);
 });
-test('The email field must be a valid email address when registration', function () {
+test('Enter an invalid email address in the email field when registration', function () {
     $registration = postJson('/api/auth/register', [
         "name" => "MT TECHNOLOGIES",
         "mobile" => "01700000001",
@@ -162,7 +242,7 @@ test('The email field must be a valid email address when registration', function
     expect($registration['message'])->toBe('The email field must be a valid email address.');
     //assertStatus(201);
 });
-test('registration', function () {
+test('Test that the user can see a confirmation message after a successful registration', function () {
     postJson('/api/auth/register', [
             "name" => "MT TECHNOLOGIES",
             "mobile" => "01700000001",

07ec1ee5f43cb228a7cf24ed8fccad625cb47f31
hafijul233 <hafijul233@users.noreply.github.com>
2023-10-26 19:46:11 +0000

    Fix styling

--- src/Models/User.php
+++ src/Models/User.php
@@ -3,14 +3,12 @@
 namespace Fintech\Auth\Models;
 
 use Fintech\Core\Traits\AuditableTrait;
-use Fintech\Core\Traits\HasUploadFiles;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Spatie\Image\Manipulations;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;

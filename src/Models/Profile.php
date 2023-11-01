454ae5e2095094fa332e615f2285a2143bc130d0
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-11-02 02:37:22 +0600

    LP-131 user proof of address added to user profile

--- src/Models/Profile.php
+++ src/Models/Profile.php
@@ -39,13 +39,42 @@ class Profile extends Model implements HasMedia
         $this->addMediaCollection('documents')
             ->acceptsMimeTypes(['image/jpeg', 'image/png', 'image/jpg', 'application/pdf'])
             ->useDisk(config('filesystems.default', 'public'));
+
+        $this->addMediaCollection('proof_of_address')
+            ->acceptsMimeTypes(['image/jpeg', 'image/png', 'image/jpg', 'application/pdf'])
+            ->useDisk(config('filesystems.default', 'public'));
     }
 
     /**
+     * this method is used by media library to upload file
+     *
      * @param $file
      * @return array<string, array>
      */
     public function documentsMediaResolve($file)
+    {
+        $attributes = ['type' => $file['type']];
+
+        if (isset($file['front'])) {
+            $attributes['side'] = "front";
+            return [$file['front'], $attributes];
+        }
+
+        if (isset($file['back'])) {
+            $attributes['side'] = "back";
+            return [$file['back'], $attributes];
+        }
+
+        return [$file, $attributes];
+    }
+
+    /**
+     * this method is used by media library to upload file
+     *
+     * @param $file
+     * @return array<string, array>
+     */
+    public function proofOfAddressMediaResolve($file)
     {
         $attributes = ['type' => $file['type']];
 

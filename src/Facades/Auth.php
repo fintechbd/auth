09854a3944ef4d0db09e8d7ac836339ac8f3d3e1
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-11-04 14:46:42 +0600

    facade class doc support added

--- src/Facades/Auth.php
+++ src/Facades/Auth.php
@@ -5,8 +5,6 @@ namespace Fintech\Auth\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @see \Fintech\Auth\Auth
- *
  * @method static \Fintech\Auth\Services\UserService user()
  * @method static \Fintech\Auth\Services\ProfileService profile()
  * @method static \Fintech\Auth\Services\RoleService role()
@@ -15,6 +13,9 @@ use Illuminate\Support\Facades\Facade;
  * @method static \Fintech\Auth\Services\OneTimePinService otp()
  * @method static \Fintech\Auth\Services\PasswordResetService passwordReset()
  * @method static \Fintech\Auth\Services\PinResetService pinReset()
+ * // Crud Service Method Point Do not Remove //
+ *
+ * @see \Fintech\Auth\Auth
  */
 class Auth extends Facade
 {

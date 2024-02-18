0fa127bc10a8607ef06cdf2ef4a7574a4c27ab34
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-02-19 02:51:48 +0600

    event list updated

--- src/EventServiceProvider.php
+++ src/EventServiceProvider.php
@@ -2,14 +2,16 @@
 
 namespace Fintech\Auth;
 
-use App\Events\AddToFavouriteAccepted;
-use App\Events\AddToFavouriteRequested;
-use Fintech\Auth\Events\PasswordResetRequested;
-use Fintech\Auth\Events\PasswordResetSuccessful;
 use Fintech\Auth\Events\AccountFreezed;
+use Fintech\Auth\Events\AddToFavouriteAccepted;
+use Fintech\Auth\Events\AddToFavouriteRejected;
+use Fintech\Auth\Events\AddToFavouriteRequested;
 use Fintech\Auth\Events\LoggedIn;
 use Fintech\Auth\Events\LoggedOut;
+use Fintech\Auth\Events\PasswordResetRequested;
+use Fintech\Auth\Events\PasswordResetSuccessful;
 use Fintech\Auth\Events\VerificationRequested;
+use Fintech\Core\Listeners\Bell\TriggerListener;
 use Illuminate\Auth\Events\Lockout;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
@@ -22,31 +24,34 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         Lockout::class => [
-//            LockoutEventListener::class,
+            TriggerListener::class
         ],
         PasswordResetRequested::class => [
-
+            TriggerListener::class
         ],
         PasswordResetSuccessful::class => [
-
+            TriggerListener::class
         ],
         AccountFreezed::class => [
-
+            TriggerListener::class
         ],
         LoggedIn::class => [
-
+            TriggerListener::class
         ],
         LoggedOut::class => [
-
+            TriggerListener::class
         ],
         VerificationRequested::class => [
-
+            TriggerListener::class
         ],
         AddToFavouriteRequested::class => [
-
+            TriggerListener::class
         ],
         AddToFavouriteAccepted::class => [
-
+            TriggerListener::class
+        ],
+        AddToFavouriteRejected::class => [
+            TriggerListener::class
         ]
     ];
 }

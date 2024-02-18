981535cdecad7a57d061ba22fafc0be612d955e4
hafijul233 <hafijul233@users.noreply.github.com>
2024-02-18 21:46:30 +0000

    Fix styling

--- src/Events/AddToFavouriteAccepted.php
+++ src/Events/AddToFavouriteAccepted.php
@@ -9,7 +9,9 @@ use Illuminate\Queue\SerializesModels;
 
 class AddToFavouriteAccepted
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
 
     /**
      * Create a new event instance.
--- src/Events/AddToFavouriteRequested.php
+++ src/Events/AddToFavouriteRequested.php
@@ -9,7 +9,9 @@ use Illuminate\Queue\SerializesModels;
 
 class AddToFavouriteRequested
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
 
     /**
      * Create a new event instance.

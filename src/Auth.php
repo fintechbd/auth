15d31610b095be358abfae41b49ea094581ef7a6
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-20 02:37:56 +0600

    unused php doc block removed

--- src/Auth.php
+++ src/Auth.php
@@ -17,102 +17,66 @@ use Fintech\Auth\Services\UserService;
 
 class Auth
 {
-    /**
-     * @return UserService
-     *
-     */
+
     public function user($filters = null)
     {
         return \singleton(UserService::class, $filters);
     }
 
-    /**
-     * @return UserService
-     *
-     */
+
     public function profile($filters = null)
     {
         return \singleton(ProfileService::class, $filters);
     }
 
-    /**
-     * @return RoleService
-     *
-     */
+
     public function role($filters = null)
     {
         return \singleton(RoleService::class, $filters);
     }
 
-    /**
-     * @return PermissionService
-     *
-     */
+
     public function permission($filters = null)
     {
         return \singleton(PermissionService::class, $filters);
     }
 
-    /**
-     * @return TeamService
-     *
-     */
+
     public function team($filters = null)
     {
         return \singleton(TeamService::class, $filters);
     }
 
-    /**
-     * @return OneTimePinService
-     */
     public function otp($filters = null)
     {
         return \singleton(OneTimePinService::class, $filters);
     }
 
-    /**
-     * @return PasswordResetService
-     */
     public function passwordReset($filters = null)
     {
         return \singleton(PasswordResetService::class, $filters);
     }
 
-    /**
-     * @return PinResetService
-     */
     public function pinReset($filters = null)
     {
         return \singleton(PinResetService::class, $filters);
     }
 
-    /**
-     * @return AuditService
-     */
     public function audit($filters = null)
     {
         return \singleton(AuditService::class, $filters);
     }
 
-    /**
-     * @return FavouriteService
-     */
     public function favourite($filters = null)
     {
         return \singleton(FavouriteService::class, $filters);
     }
 
-    /**
-     * @return GeoIpService
-     */
     public function geoip($filters = null)
     {
         return \singleton(GeoIpService::class, $filters);
     }
 
-    /**
-     * @return LoginAttemptService
-     */
     public function loginAttempt($filters = null)
     {
         return \singleton(LoginAttemptService::class, $filters);

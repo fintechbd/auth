76a44bcef96d7e09336512fb1322f93a3f1d4ba0
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-11-05 12:11:25 +0600

    risk profile added in user table

---
+++ database/migrations/2024_11_05_060442_update_add_risk_profile_column_in_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Fintech\Core\Enums\Auth\RiskProfile;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('risk_profile')->default(RiskProfile::Low->value)->after('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('risk_profile');
+        });
+    }
+};

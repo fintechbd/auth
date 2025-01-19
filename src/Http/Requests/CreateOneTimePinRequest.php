02664261c1c1c53586f310933e6a76ed75894d7b
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-01-19 17:54:56 +0600

    system event removed

--- src/Http/Requests/CreateOneTimePinRequest.php
+++ src/Http/Requests/CreateOneTimePinRequest.php
@@ -2,12 +2,7 @@
 
 namespace Fintech\Auth\Http\Requests;
 
-use Illuminate\Auth\Events\Lockout;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Facades\RateLimiter;
-use Illuminate\Support\Str;
-use Illuminate\Validation\ValidationException;
-use Symfony\Component\HttpFoundation\Response;
 
 class CreateOneTimePinRequest extends FormRequest
 {
@@ -29,53 +24,5 @@ class CreateOneTimePinRequest extends FormRequest
             'email' => ['required_without_all:mobile,user', 'string', 'email:rfc,dns'],
             'user' => ['required_without_all:mobile,email', 'integer', 'min:1'],
         ];
-    }
-
-    /**
-     * clear the rate limiter if authenticated
-     */
-    public function clearRateLimited(): void
-    {
-        RateLimiter::clear($this->throttleKey());
-    }
-
-    /**
-     * Get the rate limiting throttle key for the request.
-     */
-    public function throttleKey(): string
-    {
-        $key = $this->input('mobile', $this->input('email', $this->input('user')));
-
-        return Str::transliterate(Str::lower($key.'|'.$this->ip()));
-    }
-
-    /**
-     * count the rate limiter if authenticated
-     */
-    public function hitRateLimited(): void
-    {
-        RateLimiter::hit($this->throttleKey());
-    }
-
-    /**
-     * Ensure the login request is not rate limited.
-     *
-     * @throws ValidationException
-     */
-    public function ensureIsNotRateLimited(): void
-    {
-        if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
-            return;
-        }
-
-        event(new Lockout($this));
-
-        $seconds = RateLimiter::availableIn($this->throttleKey());
-
-        abort(Response::HTTP_TOO_MANY_REQUESTS, __('auth::messages.throttle', [
-            'seconds' => $seconds,
-            'minutes' => ceil($seconds / 60),
-        ]));
-
     }
 }
--- src/Http/Requests/UserAuthResetRequest.php
+++ src/Http/Requests/UserAuthResetRequest.php
@@ -2,12 +2,7 @@
 
 namespace Fintech\Auth\Http\Requests;
 
-use Illuminate\Auth\Events\Lockout;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Facades\RateLimiter;
-use Illuminate\Support\Str;
-use Illuminate\Validation\ValidationException;
-use Symfony\Component\HttpFoundation\Response;
 
 class UserAuthResetRequest extends FormRequest
 {
@@ -27,51 +22,5 @@ class UserAuthResetRequest extends FormRequest
         return [
             config('fintech.auth.password_field', 'password') => config('fintech.auth.password_field_rules', ['required', 'string', 'min:8']),
         ];
-    }
-
-    /**
-     * clear the rate limiter if authenticated
-     */
-    public function clearRateLimited(): void
-    {
-        RateLimiter::clear($this->throttleKey());
-    }
-
-    /**
-     * Get the rate limiting throttle key for the request.
-     */
-    public function throttleKey(): string
-    {
-        return Str::transliterate(Str::lower($this->input(config('fintech.auth.auth_field', 'login_id'))).'|'.$this->ip());
-    }
-
-    /**
-     * count the rate limiter if authenticated
-     */
-    public function hitRateLimited(): void
-    {
-        RateLimiter::hit($this->throttleKey());
-    }
-
-    /**
-     * Ensure the login request is not rate limited.
-     *
-     * @throws ValidationException
-     */
-    public function ensureIsNotRateLimited(): void
-    {
-        if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
-            return;
-        }
-
-        event(new Lockout($this));
-
-        $seconds = RateLimiter::availableIn($this->throttleKey());
-
-        abort(Response::HTTP_TOO_MANY_REQUESTS, __('auth::messages.throttle', [
-            'seconds' => $seconds,
-            'minutes' => ceil($seconds / 60),
-        ]));
-
     }
 }

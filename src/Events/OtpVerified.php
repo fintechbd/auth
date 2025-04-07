f66ad50846f38f843fbf8593840357469e9b810f
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-04-07 18:38:24 +0600

    otp verification completed

--- src/Events/OtpVerified.php
+++ src/Events/OtpVerified.php
@@ -2,24 +2,28 @@
 
 namespace Fintech\Auth\Events;
 
+use Fintech\Auth\Facades\Auth;
 use Fintech\Core\Abstracts\BaseEvent;
 use Fintech\Core\Attributes\ListenByTrigger;
 use Fintech\Core\Attributes\Variable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Notifications\AnonymousNotifiable;
 use Illuminate\Queue\SerializesModels;
 
 #[ListenByTrigger(
     name: 'OTP Verified',
-    description: 'Trigger fires when system delete the user account and related information.',
+    description: 'Trigger fires when system verify the account identify and related information.',
     enabled: true,
     anonymous: true,
     variables: [
-        new Variable(name: '__account_name__', description: 'Name of the user tried login'),
-        new Variable(name: '__account_mobile__', description: 'Mobile number associate with requested user'),
-        new Variable(name: '__account_email__', description: 'Email address associate with requested user'),
-        new Variable(name: '__account_status__', description: 'User account before frozen/suspended status.'),
+        new Variable(name: '__otp__', description: 'The Random number was sent for verification'),
+        new Variable(name: '__channel__', description: 'The Medium how verification was done.'),
+        new Variable(name: '__created_at__', description: 'Datetime of the verification was created.'),
+        new Variable(name: '__verified_at__', description: 'Datetime of the verification was completed.'),
+        new Variable(name: '__account_identity__', description: 'The information which was verified.'),
         new Variable(name: '__ip__', description: 'IP address of the request received'),
+        new Variable(name: '__platform__', description: 'User Agent/Platform of the trigger received'),
     ]
 )]
 class OtpVerified extends BaseEvent
@@ -39,11 +43,10 @@ class OtpVerified extends BaseEvent
 
     public function user(): mixed
     {
-
-        return match ($this->otpModel['auth_key']) {
-//            'user' => Auth::user()->find($this->otpInfo['auth_value']),
-//            'email' => (new AnonymousNotifiable())->route('mail', $this->otpInfo['auth_value']),
-//            'mobile' => (new AnonymousNotifiable())->route('sms', $this->otpInfo['auth_value']),
+        return match ($this->otpModel->channel) {
+            'user' => Auth::user()->find($this->otpModel->email),
+            'email' => (new AnonymousNotifiable())->route('mail', $this->otpModel->email),
+            'mobile' => (new AnonymousNotifiable())->route('sms', $this->otpModel->email),
             default => null,
         };
     }
@@ -55,10 +58,11 @@ class OtpVerified extends BaseEvent
     public function aliases(): array
     {
         return [
-            '__account_name__' => $this->user->name ?? null,
-            '__account_mobile__' => $this->user->mobile ?? null,
-            '__account_email__' => $this->user->email ?? null,
-            '__account_status__' => $this->user->status ? ucfirst($this->user->status) : null,
+            '__otp__' => $this->otpModel->token,
+            '__channel__' => ucfirst($this->otpModel->channel),
+            '__created_at__' => $this->otpModel->created_at->format('c'),
+            '__verified_at__' => now()->format('c'),
+            '__account_identity__' => now()->format('c'),
             '__ip__' => request()->ip(),
             '__platform__' => request()->userAgent(),
         ];

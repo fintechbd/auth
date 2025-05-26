0a94da4b94b04f533cfe1a2053ec7aedaa872ee8
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-05-26 17:22:55 +0600

    phpdoc block updated

--- src/Auth.php
+++ src/Auth.php
@@ -14,68 +14,114 @@ use Fintech\Auth\Services\ProfileService;
 use Fintech\Auth\Services\RoleService;
 use Fintech\Auth\Services\TeamService;
 use Fintech\Auth\Services\UserService;
+use Fintech\Core\Abstracts\BaseModel;
+use Illuminate\Database\Eloquent\Collection;
 
 class Auth
 {
+    /**
+     * @param $filters
+     * @return UserService|Collection|BaseModel
+     */
     public function user($filters = null)
     {
         return \singleton(UserService::class, $filters);
     }
 
-
+    /**
+     * @param $filters
+     * @return ProfileService|Collection|BaseModel
+     */
     public function profile($filters = null)
     {
         return \singleton(ProfileService::class, $filters);
     }
 
-
+    /**
+     * @param $filters
+     * @return RoleService|Collection|BaseModel
+     */
     public function role($filters = null)
     {
         return \singleton(RoleService::class, $filters);
     }
 
-
+    /**
+     * @param $filters
+     * @return PermissionService|Collection|BaseModel
+     */
     public function permission($filters = null)
     {
         return \singleton(PermissionService::class, $filters);
     }
 
-
+    /**
+     * @param $filters
+     * @return TeamService|Collection|BaseModel
+     */
     public function team($filters = null)
     {
         return \singleton(TeamService::class, $filters);
     }
 
+    /**
+     * @param $filters
+     * @return OneTimePinService|Collection|BaseModel
+     */
     public function otp($filters = null)
     {
         return \singleton(OneTimePinService::class, $filters);
     }
 
+    /**
+     * @param $filters
+     * @return PasswordResetService|Collection|BaseModel
+     */
     public function passwordReset($filters = null)
     {
         return \singleton(PasswordResetService::class, $filters);
     }
 
+    /**
+     * @param $filters
+     * @return PinResetService|Collection|BaseModel
+     */
     public function pinReset($filters = null)
     {
         return \singleton(PinResetService::class, $filters);
     }
 
+    /**
+     * @param $filters
+     * @return AuditService|Collection|BaseModel
+     */
     public function audit($filters = null)
     {
         return \singleton(AuditService::class, $filters);
     }
 
+    /**
+     * @param $filters
+     * @return FavouriteService|Collection|BaseModel
+     */
     public function favourite($filters = null)
     {
         return \singleton(FavouriteService::class, $filters);
     }
 
+    /**
+     * @param $filters
+     * @return GeoIpService|Collection|BaseModel
+     */
     public function geoip()
     {
         return \singleton(GeoIpService::class);
     }
 
+    /**
+     * @param $filters
+     * @return LoginAttemptService|Collection|BaseModel
+     */
     public function loginAttempt($filters = null)
     {
         return \singleton(LoginAttemptService::class, $filters);
